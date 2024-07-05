9190ae8a3c1597d980743bb039124795a8fa9456
Arne Olemans <Arne.olemans@telenet.be>
2024-07-05 20:52:25 +0200

    Drawing on image

--- OpenCVCourse/Chapter4.cpp
+++ OpenCVCourse/Chapter4.cpp
@@ -11,12 +11,17 @@ int main()
 
 	cv::Mat img(512, 512, CV_8UC3, cv::Scalar(255, 255, 255));
 
+	std::string path = "Resources/test.png";
+	cv::Mat img2 = cv::imread(path);
+
 	cv::circle(img, cv::Point(256, 256), 155, cv::Scalar(0, 69, 255), cv::FILLED);
 	cv::rectangle(img, cv::Point(130, 226), cv::Point(382, 286), cv::Scalar(255, 255, 255), cv::FILLED);
 	cv::line(img, cv::Point(130, 296), cv::Point(382, 296), cv::Scalar(255, 255, 255), 2);
+	cv::line(img2, cv::Point(130, 296), cv::Point(382, 296), cv::Scalar(255, 255, 5), 2);
 
 	cv::putText(img, "Murtaza's Workshop", cv::Point(137, 262), cv::FONT_HERSHEY_DUPLEX, 0.75, cv::Scalar(0, 69, 255), 2);
 
 	cv::imshow("Image", img);
-	cv::waitKey(0);
+	cv::imshow("Image2", img2);
+	cv::waitKey(waitTime);
 }
